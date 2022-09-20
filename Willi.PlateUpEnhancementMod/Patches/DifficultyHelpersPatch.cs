c3716051fdc65f4ffb84cdc9dbca7dc0a226f512
WillWilli <113902340+WillWilli@users.noreply.github.com>
2022-09-20 08:09:55 +0100

    prefer postfix

--- Willi.PlateUpEnhancementMod/Patches/DifficultyHelpersPatch.cs
+++ Willi.PlateUpEnhancementMod/Patches/DifficultyHelpersPatch.cs
@@ -22,12 +22,12 @@ namespace Willi.PlateUpEnhancementMod.Patches
         }
 
         [HarmonyPatch("BaseUpgradedShopChance")]
-        [HarmonyPrefix]
-        public static bool UpgradedShopChancePrefix(int day, ref float __result)
+        [HarmonyPostfix]
+        public static bool UpgradedShopChance_Postfix(int day, ref float __result)
         {
             if (DefaultShopOverrideSettings.Value)
             {
-                __result = DefaultShopUpgradedChance.Value;
+                __result = __result * DefaultShopUpgradedChance.Value;
                 return false;
             }
 
@@ -36,16 +36,16 @@ namespace Willi.PlateUpEnhancementMod.Patches
 
         [HarmonyPatch("MoneyRewardPlayerModifier")]
         [HarmonyPostfix]
-        public static void MoneyRewardPlayerModifierPostfix(ref float __result, int player_count)
+        public static void MoneyRewardPlayerModifier_Postfix(ref float __result, int player_count)
         {
-            __result = MoneyRewardMultiplier.Value;
+            __result *= MoneyRewardMultiplier.Value;
         }
 
         [HarmonyPatch("CustomerModifierRateModifier")]
         [HarmonyPostfix]
         public static void CustomerModifierRateModifierPrefix(ref float __result)
         {
-            __result = NumberOfCustomersMultiplier.Value;
+            __result *= NumberOfCustomersMultiplier.Value;
         }
     }
 }
