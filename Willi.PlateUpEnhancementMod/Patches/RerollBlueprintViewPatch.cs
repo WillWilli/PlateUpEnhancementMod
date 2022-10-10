db1a36f81f29c03f0db5016fd2d95b3195cee7ac
WillWilli <113902340+WillWilli@users.noreply.github.com>
2022-10-10 18:06:49 +0100

    Add blueprint override reroll code

---
+++ Willi.PlateUpEnhancementMod/Patches/RerollBlueprintViewPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Kitchen;
+using Unity.Entities;
+using static Willi.PlateUpEnhancementMod.Config.ConfigHelper;
+
+namespace Willi.PlateUpEnhancementMod.Patches
+{
+    [HarmonyPatch(typeof(RerollBlueprintView))]
+    public static class RerollBlueprintViewPatch
+    {
+        private static EntityManager _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        [HarmonyPatch("UpdateData")]
+        [HarmonyPostfix]
+        public static void UpdateData_Prefix()
+        {
+            if (RerollShopFixedCost.Value < 0)
+                return;
+
+            if (_entityManager == null)
+                _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            var eq = _entityManager.CreateEntityQuery((ComponentType)typeof(SRerollCost));
+            eq.SetSingleton<SRerollCost>(new SRerollCost() { Cost = RerollShopFixedCost.Value });
+        }
+    }
+}
